ff2d22b008b3c88b2edcfeadd06f79cc7331091e
Alex Sklyarenko <cashruntome@gmail.com>
2017-11-10 11:28:22 +0200

    apns

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+    ios: {
+      token: {
+        key: '/apns/SUYCNDVN8M.p8',
+        keyId: "SUYCNDVN8M",
+        teamId: "HJP269YLH5" // The Team ID for your developer account
+      },
+      topic: 'ca.shared.SharedNews', // The bundle identifier associated with your app
+      production: false
+    }
+  },
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
