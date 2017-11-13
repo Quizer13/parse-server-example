9c3849a406e15b9378a7ee08abf7e35a89f5499b
Alex Sklyarenko <cashruntome@gmail.com>
2017-11-13 15:02:58 +0200

    fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var AnalyticsAdapter = require('parse-server/lib/Adapters/Analytics/AnalyticsAdapter');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -20,7 +19,6 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',
   scheduledPush: true,
   verifyUserEmails: true,
-  analyticsAdapter: AnalyticsAdapter,
   verbose: true,
   push: {
     ios: {
